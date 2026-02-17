aba3d82a82721acad76c6c0d8b87228aa54bebec
Suveenaaa12 <suveenamogasala@gmail.com>
2026-02-17 18:38:13 +0530

    Add frontend folder or submodule

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import Createuser from './Pages/Admin/createuser';
+import AddMembership from './Pages/Admin/addmembership';
+import AddTournament from './Pages/Admin/addtournment';
+import StudentDataManagement from './Pages/Admin/studentdatamanagemant';
+import Payment from './Pages/Admin/payment';
+import CertificateGenerate from './Pages/Admin/certificategenerate';
+import Reports from './Pages/Admin/reports';
+import UserManagement from "./Pages/Sub-Admin/usermanagement";
 
-function App() {
+
+function App() {    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Createuser />} />
+      <Route path="/addmembership" element={<AddMembership />} />
+      <Route path="/addtournament" element={<AddTournament />} />
+      <Route path="/studentdatamanagement" element={<StudentDataManagement />} />
+      <Route path="/payment" element={<Payment />} />
+      <Route path="/certificategenerate" element={<CertificateGenerate />} />
+      <Route path="/reports" element={<Reports />} />
+      <Route path="/usermanagement" element={<UserManagement />} />
+    </Routes>
+    </BrowserRouter>
+
   );
-}
+}   
 
 export default App;
+
+
